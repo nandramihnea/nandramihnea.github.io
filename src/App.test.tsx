1da95994a14168dffd684d7dcd53816e1dd83440
Nandra Mihnea <nandra.mihnea@gmail.com>
2024-06-15 14:08:08 +0300

    react-testing-library added

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+
+import App from "./App";
+
+it("should have hello world", () => {
+  render(<App />);
+  const message = screen.queryByText(/Sall/i);
+  expect(message).toBeVisible();
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import { Button } from "./components/ui/button";
 
 function App() {
-  return (
-    <div className="text-center">
-      Sall<Button variant="destructive">Sall</Button>
-    </div>
-  );
+  return <div className="text-center">Sall</div>;
 }
 
 export default App;
