c923c6b1af4494a7f5b60e97a99d1b8167f723bf
Nandra Mihnea <nandra.mihnea@gmail.com>
2024-06-15 12:19:10 +0300

    added shadcn lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <div className="text-center">Salutare</div>;
+  return (
+    <div className="text-center">
+      Sall<Button variant="destructive">Sall</Button>
+    </div>
+  );
 }
 
 export default App;
